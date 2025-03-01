6b89bbf27c6fddcba1cb57d8b4ba66f188de7d3f
Maciej <maprz2017@gmail.com>
2025-03-01 21:02:56 +0100

    Added taking damage particle effect to Jeff

--- la_creatura_game/src/test_elements/enemy/jeff.gd
+++ la_creatura_game/src/test_elements/enemy/jeff.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var hurtbox_collision: CollisionShape2D = $Hurtbox/CollisionShape2D
 @onready var hp_regen_timer: Timer = $HPRegenTimer
+@onready var cpu_particles_2d: CPUParticles2D = $CPUParticles2D
 
 var max_hp: int = 5
 var hp: int
@@ -18,6 +19,7 @@ func _process(delta: float) -> void:
 func _on_hurtbox_area_entered(area: Area2D) -> void:
 	if area.is_in_group("wing_attack") and hp > 0:
 		hp -= 1
+		cpu_particles_2d.emitting = true
 	print(hp)
 
 func _on_hp_regen_timer_timeout() -> void:
