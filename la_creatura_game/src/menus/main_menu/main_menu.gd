7bacde2e270084a09f962e0324cbcb5558e9a7ad
Alina Łucja Osińska <werylilpersona@gmail.com>
2025-04-06 15:14:12 +0200

    main screen

---
+++ la_creatura_game/src/menus/main_menu/main_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+@onready var main: Node = $"../.."
+
+func _ready() -> void:
+	get_tree().paused=true
+
+func _on_exit_button_down() -> void:
+	main.get_tree().quit()
+
+
+func _on_play_button_down() -> void:
+	hide()
+	get_tree().paused=false
