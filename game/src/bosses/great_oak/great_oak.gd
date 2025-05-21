c2e7e82d48f6495679aa381da0e3c6e9856ccc1c
Maciej <maprz2017@gmail.com>
2025-05-22 00:47:07 +0200

    Changed attack patterns a bit

--- game/src/bosses/great_oak/great_oak.gd
+++ game/src/bosses/great_oak/great_oak.gd
@@ -36,14 +36,14 @@ const dmg_dictionary = { # Disctionary used to determine the dmg taken by the pl
 # Attack patterns
 const attack_patterns1 = [
 	[2, 3, 1, 0],
-	[2, 4, 2, 5, 0],
+	[1, 4, 2, 5, 0],
 	[4, 5, 2, 1, 4, 0]
 ]
 
 const attack_patterns2 = [
-	[3, 4, 3, 2, 0],
+	[3, 4, 3, 1, 0],
 	[5, 3, 2, 1, 3, 0],
-	[4, 3, 2, 5, 2, 1, 0]
+	[4, 3, 1, 5, 2, 1, 0]
 ]
 
 # Dynamic variables
