55826f3e1c0429c42bbfc4779e0a209fb5acf649
Cron-lot <modziarzpl@gmail.com>
2025-07-06 23:31:54 +0200

    gfdfgdd

--- game/src/menus/main_menu/main_menu.gd
+++ game/src/menus/main_menu/main_menu.gd
@@ -1,6 +1,8 @@
 extends Control
 
 @export var gameplay: Node2D
+@onready var dialogue_interface: Control = $"../../Gameplay/UI/UI/DialogueInterface"
+@onready var ui: Control = $"../../Gameplay/UI/UI"
 
 func _on_exit_button_down() -> void:
 	get_tree().quit()
@@ -8,5 +10,8 @@ func _on_exit_button_down() -> void:
 func _on_play_button_down() -> void:
 	hide()
 	get_tree().paused = false
+	dialogue_interface.show()
+	ui.dialogue_scene = ui.get_scene("#Obraz pierwszy")
+	ui.print_scene()
 	if gameplay.hp <= 0:
 		get_tree().reload_current_scene()
